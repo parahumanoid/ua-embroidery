3040d74b0fa874c1719c5ba5f571a0e00b9ddff2
parahumanoid <57003748+parahumanoid@users.noreply.github.com>
2024-05-28 17:54:47 +0000

    Deploying to gh-pages from @ parahumanoid/ua-embroidery@c8c7949afd4ead1e56cd6487134a7c4afc3d600f 🚀

---
+++ assets/tesimonials-84570235.js
@@ -0,0 +1,2 @@
+$(".customer-list").slick({dots:!1,arrows:!1,autoplay:!0,speed:400,infinite:!0,slidesToShow:1,slidesToScroll:1,mobileFirst:!0,responsive:[{breakpoint:767,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:1279,settings:{slidesToShow:3,slidesToScroll:2}}]});
+//# sourceMappingURL=tesimonials-84570235.js.map
